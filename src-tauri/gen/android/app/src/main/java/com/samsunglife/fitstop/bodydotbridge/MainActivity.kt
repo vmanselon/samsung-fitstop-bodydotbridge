e73069cff03be8884a4637cf0282288b39e0ef0e
victor <v.manselon@gmail.com>
2026-09-16 21:24:07 +0900

    test build apk

---
+++ src-tauri/gen/android/app/src/main/java/com/samsunglife/fitstop/bodydotbridge/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.samsunglife.fitstop.bodydotbridge
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+
+class MainActivity : TauriActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    enableEdgeToEdge()
+    super.onCreate(savedInstanceState)
+  }
+}
